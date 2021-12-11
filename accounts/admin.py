a8295a505e209c8e0139b144ef3126903a485398
moostofa <mmdrahinz96@gmail.com>
2021-12-12 00:27:15 +1100

    successfully implemented class-based LoginView

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Book, Anime, Manga
 
-# Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register(Book)
+admin.site.register(Anime)
+admin.site.register(Manga)
