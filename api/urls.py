d1b7dfcd93d6588c38e022397da5b546b0e0226b
moostofa <mmdrahinz96@gmail.com>
2021-12-13 11:36:24 +1100

    created urls to books, anime and manga pages

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("books", views.books, name="books"),
+    path("anime", views.anime, name="anime"),
+    path("manga", views.manga, name="manga")
 ]
--- api/views.py
+++ api/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
 
-# Create your views here.
 def index(request):
     user = request.user.username if request.user.is_authenticated else "Not logged in"
     return HttpResponse(f"Hello world! User: {user}")
+
+
+def books(request):
+    return HttpResponse("<h1>Books page</h1>")
+
+
+def anime(request):
+    return HttpResponse("<h1>Anime page</h1>")
+
+
+def manga(request):
+    return HttpResponse("<h1>Manga page</h1>")
