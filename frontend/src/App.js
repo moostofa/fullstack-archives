7b36dd685e10f644a200e9f82a9395273dc7fb44
moostofa <mmdrahinz96@gmail.com>
2021-12-14 10:40:16 +1100

    installed & successfully created react routers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,17 @@ import React from 'react'
 import './App.css';
 import HomePage from './components/HomePage';
 import Search from './components/Search';
+import { Link, Outlet } from "react-router-dom"
 
 const App = () => {
 	const items = ["Books", "Anime", "Manga"]
 	return (
-	<div>
-	{
-		items.map(item => (
-			<HomePage name={item} />
-		))
-	}
-	<Search />
-	</div>
+		<div>{
+			items.map(item => (
+				<HomePage name={item} />
+			))}
+		<Link to="/books">Books</Link>
+		</div>
 	)
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,20 @@
+import {
+	HashRouter,
+	Routes,
+	Route
+} from "react-router-dom"
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Books from "./routes/Books";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<HashRouter>
+	<Routes>
+		<Route exact path="/" element={<App />} />
+		<Route path="books" element={<Books />} />
+	</Routes>
+	</HashRouter>,
+	document.getElementById('root')
 );
