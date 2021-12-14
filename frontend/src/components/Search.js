2ee952e7ccc730e35530f9664cffd3b979427c0e
moostofa <mmdrahinz96@gmail.com>
2021-12-14 13:46:37 +1100

    perform search & store q and search results in state

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 
-const Search = () => {
+const Search = (props) => {
     const [state, setstate] = useState({"items": []})
 
     const searchBooks = () => {
--- frontend/src/routes/Books.js
+++ frontend/src/routes/Books.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Search from '../components/Search'
 
 const Books = () => {
+    const [state, setstate] = useState({
+        "q": "",
+        "results": ""
+    })
+
+    const performSearch = event => {
+        event.preventDefault()
+        setstate({
+            "q": event.target.q.value,
+            "results": <Search />
+        })
+    }
+
     return (
         <div>
-            <form>
-                <input type="text" placeholder='Book title' />
+            <form onSubmit={performSearch}>
+                <input name='q' type="text" placeholder='Book title' />
                 <input type="submit" value="Search" />
             </form>
+            <p>The value of q: {state["q"]}</p>
+            <h1>Results</h1>
+            <div>
+                {state.results}
+            </div>
         </div>
     )
 }
