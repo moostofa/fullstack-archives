1fcefd9130f49339501eecb17f341b405d4e46c9
moostofa <mmdrahinz96@gmail.com>
2021-12-16 12:02:59 +1100

    change param :category to :subject for routes

---
+++ frontend/src/routes/Subjects.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react'
+import { useParams } from "react-router-dom"
+import SearchForm from '../components/SearchForm'
+
+const subjectOptions = ["books", "anime", "manga"]
+
+const Subjects = () => {
+    const [state, setstate] = useState("")
+    const subject = useParams().subject
+
+    useEffect(() => {
+        setstate(
+            subjectOptions.includes(subject)
+            ? <SearchForm 
+                subject = {subject.charAt(0).toLocaleUpperCase() + subject.slice(1)} 
+              />
+            : <h1>A "{subject}" item cannot be searched.</h1>
+        )
+    }, [subject])
+
+    return (
+        <div> {state} </div>
+    )
+}
+
+export default Subjects
