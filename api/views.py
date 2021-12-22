543ee78c3e1bc1648b98756111b24d441f7ba34b
moostofa <mmdrahinz96@gmail.com>
2021-12-22 11:54:20 +1100

    install corsheaders

--- api/views.py
+++ api/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render
-from django.http.response import HttpResponse, JsonResponse
+from django.http.response import HttpResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 from .models import Book, Anime, Manga
 from .seralizers import ItemListSerializer
 
-from archives.settings import GOOGLE_BOOKS_API_KEY
-
 def index(request):
-    return HttpResponse(GOOGLE_BOOKS_API_KEY)
+    return HttpResponse("index")
 
 
 @api_view(["GET"])
 def books(request):
-    return JsonResponse({"ApiKey": GOOGLE_BOOKS_API_KEY})
+    books = Book.objects.all()
+    serializer = ItemListSerializer(books, many=True)
+    return Response(serializer.data)
 
 
 @api_view(["GET"])
