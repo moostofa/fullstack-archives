3e0db12fc73e1c5f9028e791163070e6dcd75cd5
moostofa <mmdrahinz96@gmail.com>
2021-12-22 14:45:38 +1100

    created login form

--- frontend/src/components/accounts/Login.jsx
+++ frontend/src/components/accounts/Login.jsx
@@ -1,23 +1,36 @@
 import React, { useState, useEffect } from 'react'
+import { Button, FormControl, TextField, Grid } from '@mui/material'
 
 const Login = () => {
-    const [state, setstate] = useState({})
+    const [state, setstate] = useState({
+        username: "",
+        password1: "",
+        password2: ""
+    })
 
-    useEffect(() => {
-        testAPI()
-    }, [])
-
-    const testAPI = async () => {
-        const response = await fetch("/api/books")
-        const json = await response.json()
-        console.log(json)
-        setstate(json)
+    const handleChange = event => {
+        setstate({
+            ...state,
+            [event.target.name]: event.target.value
+        })
     }
 
     return (
-        <div>
-            {JSON.stringify(state)}
-        </div>
+        <Grid container justifyContent='center' alignItems='center' flexDirection='column' gap={1}>
+            <h1>Login</h1>
+            <Grid item>
+                <TextField name='username' label="Username" size='small' value={state.username} onChange={handleChange} />
+            </Grid>
+            <Grid item>
+                <TextField name='password1' label="Password" size='small' type='password' value={state.password1} onChange={handleChange} />
+            </Grid>
+            <Grid item>
+                <TextField name='password2' label="Confirm password" size='small' type='password' value={state.password2} onChange={handleChange} />
+            </Grid>
+            <Grid item>
+                <Button type='submit' variant='contained'>Login</Button>
+            </Grid>
+        </Grid>
     )
 }
 
