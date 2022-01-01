6510aee6d902c64cbc395bf36a10dff0ffd88eb6
moostofa <mmdrahinz96@gmail.com>
2022-01-01 16:56:05 +1100

    successfully register user and store auth token in localstorage

--- frontend/src/components/accounts/Register.jsx
+++ frontend/src/components/accounts/Register.jsx
@@ -1,19 +1,30 @@
 import React, { useEffect, useState } from 'react'
-import { Button, FormControl, TextField } from '@mui/material'
-import Cookies from 'js-cookie'
+import { useNavigate } from 'react-router-dom'
+import { Button, FormControl, FormHelperText, TextField } from '@mui/material'
 
 const Register = () => {
     // [0] is the value of the controlled TextField component
     // [1] is a boolean value indicating whether the TextField should display an error text or not
     const [credentials, setcredentials] = useState({
         username: ["", false],
-        password1: ["", false],
-        password2: ["", false]
+        password: ["", false],
+        passwordConfirm: ["", false]
+    })
+
+    // helper texts to give feedback to user if their input is invalid
+    const [helperText, setHelperText] = useState({
+        form: "",
+        username: "Username is already taken.",
+        password: "Passwords do not match.",
+        passwordConfirm: "Passwords do not match."
     })
 
     // stores a list of usernames that are already taken
     const [usernames, setUsernames] = useState([])
+    
+    const redirect = useNavigate()
 
+    // when components mounts, load and save all current usernames in state
     useEffect(() => {
         getUsernames()
     }, [])
@@ -22,13 +33,6 @@ const Register = () => {
         const response = await fetch("/auth/usernames")
         const users = await response.json()
         setUsernames(users)
-    }
-
-    // helper texts to give feedback to user if their input is invalid
-    const helperTexts = {
-        username: "Username is already taken.",
-        password1: "Passwords do not match.",
-        password2: "Passwords do not match."
     }
 
     // update credentials whenever a TextField changes
@@ -47,17 +51,28 @@ const Register = () => {
 
     // register user (send POST data to backend)
     const handleRegistration = async () => {
-        // prevent submit if username is invalid
-        if (credentials.username[1]) return
+        const username = credentials.username
+        const password = credentials.password
+        const passwordConfirm = credentials.passwordConfirm
+
+        // fields cannot be empty
+        if (username[0].trim() === "" || password[0].trim() === "" || passwordConfirm[0].trim() === "") {
+            setHelperText({
+                ...helperText,
+                form: "Please fill in the form fields."
+            })
+            return
+        }
+
+        // prevent submit if any form field is invalid
+        if (username[1] || password[1] || passwordConfirm[1]) return
 
         // validate password match
-        const password1 = credentials.password1[0]
-        const password2 = credentials.password2[0]
-        if (password1 !== password2) {
+        if (password[0] !== passwordConfirm[0]) {
             setcredentials({
                 ...credentials,
-                password1: [password1, true],
-                password2: [password2, true]
+                password: [password[0], true],
+                passwordConfirm: [passwordConfirm[0], true]
             })
             return
         }
@@ -66,24 +81,31 @@ const Register = () => {
         const response = await fetch("/auth/register", {
             method: "POST",
             body: JSON.stringify({
-                username: credentials.username[0],
-                password1,
-                password2
+                username: username[0],
+                password: password[0],
+                password_confirm: passwordConfirm[0]
             }),
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
-                'X-CSRFToken': Cookies.get("csrftoken")
               },
         })
         const result = await response.json()
-        console.log(result)
+
+        // temporary error catch
+        if (!result.success) return alert("Registration failed - Response returned 'success': False. An error was unaccounted for on client-side.")
+
+        // save user's auth token & redirect to index page
+        localStorage.removeItem("token")
+        localStorage.setItem("token", result.token)
+        redirect("/")
     }
 
     return (
         <div>
             <h1>Register</h1>
-            <FormControl> {
+            <FormControl> 
+                <FormHelperText> {helperText.form} </FormHelperText> {
                 Object.keys(credentials).map(key => (
                     <TextField 
                         key={key}
@@ -92,14 +114,14 @@ const Register = () => {
                         error={credentials[key][1]}
                         label={
                             key === "username" ? "Username"
-                            : key === "password1" ? "Password"
+                            : key === "password" ? "Password"
                             : "Confirm password"
                         }
                         type={key[0] === "p" ? "password" : "text"}
                         size='small'
                         onChange={handleChange}
                         sx={{my: 1}}
-                        helperText={credentials[key][1] ? helperTexts[key] : ""}
+                        helperText={credentials[key][1] ? helperText[key] : ""}
                     />
                 ))}
                 <Button type='submit' variant='contained' onClick={handleRegistration}>Register</Button>
