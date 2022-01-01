7cfd97881f46e2a081c22824fada5bf6240cfa23
moostofa <mmdrahinz96@gmail.com>
2022-01-01 19:26:19 +1100

    working on displaying different profile options based on auth

--- frontend/src/components/navbar/ProfileMenu.jsx
+++ frontend/src/components/navbar/ProfileMenu.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { IconButton, Menu, MenuItem } from '@mui/material'
 import { AccountCircle } from '@mui/icons-material'
@@ -6,11 +6,23 @@ import { AccountCircle } from '@mui/icons-material'
 const ProfileMenu = () => {
     // the anchor element is the element where the menu options will appear from, when the anchor element is clicked
     // open indicates whether the menu is currently being shown or not
-    const [anchorEl, setAnchorEl] = React.useState(null)
+    const [anchorEl, setAnchorEl] = useState(null)
+    const [authToken, setauthToken] = useState(localStorage.getItem("token"))
+    const [isAuthenticated, setisAuthenticated] = useState(authToken !== null)
     const open = Boolean(anchorEl)
     const redirect = useNavigate()
 
-    const routes = ["profile", "register", "login", "logout"]
+    const profileMenuOptions = [
+        ["Register", "Login"],
+        ["Profile", "Login", "Logout"]
+    ]
+
+    // update profile menu options whenever an auth token appears in localStorage (a user has logged in)
+    useEffect(() => {
+        const token = localStorage.getItem("token")
+        setauthToken(token)
+        if (token !== null) setisAuthenticated(true)
+    }, [])
     
     // display the menu when the profile icon is clicked
     const handleClick = (event) => {
@@ -21,7 +33,7 @@ const ProfileMenu = () => {
     const handleClose = (event) => {
         setAnchorEl(null)
         const redirectTo = event.currentTarget.dataset.route
-        if (redirectTo !== undefined) redirect(`accounts/${redirectTo}`)
+        if (redirectTo !== undefined) redirect(`accounts/${redirectTo.toLowerCase()}`)
     }
 
     return (
@@ -44,7 +56,7 @@ const ProfileMenu = () => {
                 'aria-labelledby': 'basic-button',
                 }}
             >{
-                routes.map(route => (
+                profileMenuOptions[isAuthenticated ? 1 : 0].map(route => (
                     <MenuItem
                         key={route} 
                         data-route={route} 
