90591246f751142133eb732f980499a00647a7e0
moostofa <mmdrahinz96@gmail.com>
2022-01-04 16:17:26 +1100

    testing returning querysets

--- api/views.py
+++ api/views.py
@@ -13,28 +13,39 @@ class AllItems(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        return Response("Return a user's entire profile.")
+        user = request.user
+        anime = Anime.objects.get(user=user)
+        books = Book.objects.get(user=user)
+        manga = Manga.objects.get(user=user)
+        users_list = anime | books | manga
+        return Response(users_list)
 
 
+# Add an item to a user's list
 class Add(APIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
+        user = request.user
         return Response("Add an item to the user's list.")
 
 
+# Delete an item from a user's list
 class Delete(APIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
+        user = request.user
         return Response("Delete an item from the user's list.")
 
 
+# Move an item from one list to another
 class Update(APIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
-    
+
     def post(self, request):
+        user = request.user
         return Response("Update an item in the user's list.")
