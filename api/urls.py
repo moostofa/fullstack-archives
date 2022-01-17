271c6a4c87f9cc87efaa4050758bc1946402c43a
moostofa <mmdrahinz96@gmail.com>
2022-01-17 12:07:23 +1100

    condensed action handlers into 1 apiview

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import AllItems, Add, Delete, Update
+from .views import AllItems, Add, Delete, Update, Action
 
 urlpatterns = [
     path("all", AllItems.as_view(), name="all"),
+    path("<str:subject>/<str:action>", Action.as_view(), name="action"),
     path("add", Add.as_view(), name="add"),
     path("delete", Delete.as_view(), name="delete"),
     path("update", Update.as_view(), name="update")
--- api/views.py
+++ api/views.py
@@ -7,44 +7,31 @@ from .models import UsersList
 from .seralizers import ItemListSerializer, ActionSerializer
 
 
-# Return a list of all of the user's items.
+""" Return a list of all of the user's items """
 class AllItems(APIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
         user = request.user
-        users_list = UsersList.objects.get(user = user)
+        users_list = UsersList.objects.get(user=user)
         serializer = ItemListSerializer(users_list)
         return Response(serializer.data)
 
 
-# Add an item to a user's list
-class Add(APIView):
+""" Perform CRUD oeprations on any of the user's lists """
+class Action(APIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
-    def post(self, request):
+    def post(self, request, *args, **kwargs):
         user = request.user
         serializer = ActionSerializer(request.data)
-        return Response(serializer.data)
-
-
-# Delete an item from a user's list
-class Delete(APIView):
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def post(self, request):
-        user = request.user
-        return Response("Delete an item from the user's list.")
-
-
-# Move an item from one list to another
-class Update(APIView):
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def post(self, request):
-        user = request.user
-        return Response("Update an item in the user's list.")
+        return Response({
+            "kwargs": {
+                "action to perform": kwargs.get("action"),
+                "subject": kwargs.get("subject")
+            },
+            "user": str(user),
+            **serializer.data
+        })
